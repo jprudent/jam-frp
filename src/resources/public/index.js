38ac52608218277352100cafb8574224222f711b
jprudent <jprudent@gmail.com>
2013-10-12 02:17:28 +0200

    step1 : map error

--- src/resources/public/index.js
+++ src/resources/public/index.js
@@ -9,11 +9,15 @@ var URL = {
 // -- Définitions des event streams et properties
 // --
 
-var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries)); // creation d'un event stream à partir de la requête ajax
+var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries)) // creation d'un event stream à partir de la requête ajax
+      .mapError("ERROR"); // en cas d'erreur produit une string spéciale
+
+var isError = function(v){
+  return v === "ERROR";
+};
 
 var isAjaxError = ajaxCountries
-  .map(false) // en cas de succes : pas de message d'erreur
-  .mapError(true) // en cas d'erreur : message d'erreur
+  .map(isError)
   .toProperty(false) // convertit en Property pour avoir une valeur initiale
   .skipDuplicates(); // si la Property vaut 2 fois false, inutile de cacher 2 fois le message d'erreur
 
