34cfc30450b09fd88477e94c8fb40a7f3c896a2f
jprudent <jprudent@gmail.com>
2013-10-12 02:08:09 +0200

    step2 : spinner

--- src/resources/public/index.js
+++ src/resources/public/index.js
@@ -1,3 +1,4 @@
+console.log("*** Strarting application ***")
 // Just an object to tidy up URLs
 var URL = {
     'countries' : 'http://localhost:3000/countries'
@@ -9,23 +10,39 @@ var URL = {
 // -- Définitions des event streams et properties
 // --
 
-var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries)); // creation d'un event stream à partir de la requête ajax
+var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries)) // creation d'un event stream à partir de la requête ajax
+  .mapError("ERROR"); // en cas d'erreur produit une string spéciale
+
+var isError = function(v){
+  return v === "ERROR";
+};
 
 var isAjaxError = ajaxCountries
-  .map(false) // en cas de succes : pas de message d'erreur
-  .mapError(true) // en cas d'erreur : message d'erreur
+  .map(isError)
   .toProperty(false) // convertit en Property pour avoir une valeur initiale
   .skipDuplicates(); // si la Property vaut 2 fois false, inutile de cacher 2 fois le message d'erreur
 
+var showAjaxSpinner = isAjaxError.awaiting(ajaxCountries);
 
 // --
 // -- side effects
 // --
 
+var showOrHide = function(show, selector){
+  if(show) selector.show();
+  else selector.hide();
+};
+
 var showOrHideErrorMessage = function(show) {
   console.log("showError = ", show);
-  if(show) $(".error").show();
-  else $(".error").hide();
-}
+  showOrHide(show, $(".error"));
+};
 
 isAjaxError.onValue(showOrHideErrorMessage);
+
+var showOrHideSpinner = function(show) {
+  console.log("showSpinner = ", show)
+  showOrHide(show, $(".spinner"));
+};
+
+showAjaxSpinner.onValue(showOrHideSpinner);
