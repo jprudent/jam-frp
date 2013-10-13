4ebcd9c57a54725ef8add2d507914da229134599
jprudent <jprudent@gmail.com>
2013-10-13 11:09:22 +0200

    step4 : retry if user click on error message

--- doc/requirements.md
+++ doc/requirements.md
@@ -1,3 +1,28 @@
 1. téléchargement de la liste des pays. Si erreur appel, affichage du message d'erreur, sinon rien.
 2. Ajout d'un spinner pendant le téléchargement
 3. Choisir un pays
+4. Quand erreur, on peut cliquer sur le message pour réessayer
+
+Hi,
+
+Say I've this EventStream  :
+var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries))
+
+If I have an error in ajax call, I would like to "restart".
+
+I know how to catch error with mapError. My point is to have another try with the ajax call without leaking memory.
+
+If I do something like :
+
+var ajaxCall = function(){
+var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries))
+ .mapError("ERROR")
+ .onValue(function(v){
+   if(v === "ERROR") ajaxCall(); //retry
+   else someSideEffect();
+  });
+}
+
+This works as expected but I think this leaks memory. What's the proper way to implement this ? returning Baco.noMore ?
+
+Thanks.
--- src/resources/public/index.js
+++ src/resources/public/index.js
@@ -1,76 +1,87 @@
-console.log("*** Strarting application ***")
-// Just an object to tidy up URLs
-var URL = {
-    'countries' : 'http://localhost:3000/countries'
-};
+var countryDefinitions = function() {
+
+  console.log("*** Strarting country definitions ***")
+
+  // Just an object to tidy up URLs
+  var URL = {
+      'countries' : 'http://localhost:3000/countries'
+  };
 
 
 
-// --
-// -- Définitions des event streams et properties
-// --
+  // --
+  // -- Définitions des event streams et properties
+  // --
 
-var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries)) // creation d'un event stream à partir de la requête ajax
-  .mapError("ERROR"); // en cas d'erreur produit une string spéciale
+  var ajaxCountries = Bacon.fromPromise($.ajax(URL.countries)) // creation d'un event stream à partir de la requête ajax
+    .mapError("ERROR"); // en cas d'erreur produit une string spéciale
 
-var isError = function(v){
-  return v === "ERROR";
-};
+  var isError = function(v){
+    return v === "ERROR";
+  };
 
-var not = function(f){
-  return function(v){ return !f(v) };
-};
+  var not = function(f){
+    return function(v){ return !f(v) };
+  };
 
-var isAjaxError = ajaxCountries
-  .map(isError)
-  .toProperty(false) // convertit en Property pour avoir une valeur initiale
-  .skipDuplicates(); // si la Property vaut 2 fois false, inutile de cacher 2 fois le message d'erreur
+  var isAjaxError = ajaxCountries
+    .map(isError)
+    .toProperty(false) // convertit en Property pour avoir une valeur initiale
+    .skipDuplicates(); // si la Property vaut 2 fois false, inutile de cacher 2 fois le message d'erreur
 
-var showAjaxSpinner = isAjaxError.awaiting(ajaxCountries);
+  var showAjaxSpinner = isAjaxError.awaiting(ajaxCountries);
 
-var showCountryInput = ajaxCountries
-  .map(not(isError))
-  .toProperty(false)
-  .skipDuplicates();
+  var showCountryInput = ajaxCountries
+    .map(not(isError))
+    .toProperty(false)
+    .skipDuplicates();
 
-var gotCountriesList = ajaxCountries
-  .filter(not(isError));
+  var gotCountriesList = ajaxCountries
+    .filter(not(isError));
 
+  var clickOnErrorMessage = $(".error").asEventStream("click");
 
-// --
-// -- side effects
-// --
+  // --
+  // -- side effects
+  // --
 
-var showOrHide = function(show, selector){
-  if(show) selector.show();
-  else selector.hide();
-};
+  var showOrHide = function(show, selector){
+    if(show) selector.show();
+    else selector.hide();
+  };
 
-var showOrHideErrorMessage = function(show) {
-  console.log("showError = ", show);
-  showOrHide(show, $(".error"));
-};
+  var showOrHideErrorMessage = function(show) {
+    console.log("showError = ", show);
+    showOrHide(show, $(".error"));
+  };
 
-isAjaxError.onValue(showOrHideErrorMessage);
+  isAjaxError.onValue(showOrHideErrorMessage);
 
-var showOrHideSpinner = function(show) {
-  console.log("showSpinner = ", show)
-  showOrHide(show, $(".spinner"));
-};
+  var showOrHideSpinner = function(show) {
+    console.log("showSpinner = ", show)
+    showOrHide(show, $(".spinner"));
+  };
 
-showAjaxSpinner.onValue(showOrHideSpinner);
+  showAjaxSpinner.onValue(showOrHideSpinner);
 
-var showOrHideInputCountry = function(show){
-   console.log("show country input", show);
-  showOrHide(show, $("[name='country']"));
+  var showOrHideInputCountry = function(show){
+     console.log("show country input", show);
+    showOrHide(show, $("[name='country']"));
+  }
+
+  showCountryInput.onValue(showOrHideInputCountry);
+
+  var fillCountries = function(countries){
+    for(var countryCode in countries){
+      $("#countries").append('<option value="'+ countries[countryCode] +'">' + countryCode + '</option>');
+    }
+  };
+
+  gotCountriesList.onValue(fillCountries);
+
+  clickOnErrorMessage.onValue(countryDefinitions);
+
+  console.log("*** End country definition ***");
 }
 
-showCountryInput.onValue(showOrHideInputCountry);
-
-var fillCountries = function(countries){
-  for(var countryCode in countries){
-    $("#countries").append('<option value="'+ countries[countryCode] +'">' + countryCode + '</option>');
-  }
-};
-
-gotCountriesList.onValue(fillCountries);
+countryDefinitions();
